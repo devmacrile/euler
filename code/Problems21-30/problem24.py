891c5bbd3a9a39f22e5c57514f33de72cb25f093
Devin Riley <rileyde@umich.edu>
2014-05-03 13:34:28 -0400

    Solution to problem 24.  Fun one

--- code/Problems21-30/problem24.py
+++ code/Problems21-30/problem24.py
@@ -1,11 +1,14 @@
 #Devin Riley
 #Project Euler
 #Problem 24
-#2/9/14
+#5/1/14
 
 
 """
-A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+A permutation is an ordered arrangement of objects. 
+For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. 
+If all of the permutations are listed numerically or alphabetically, we call it 
+lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
 
 012   021   102   120   201   210
 
@@ -14,14 +17,44 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 
 #initial thoughts:
 #Ways to permute 10 numbers = 10!
+#approach: what 9! chunk is 1000000 in?
+#           what 8! chunk?
+#            what 7! chunk?
+#             ......
  
+import time
+import math
+
 def factorial(n):
     #returns n!
-    if n == 1:
-        return n
+    if n < 0:
+        print "ERROR: cannot compute factorial of a negative number"
+        return
+    if n == 1 or n == 0:
+        return 1
     return n*factorial(n-1)
-    
-print factorial(10)
-#3,628,800
 
-#9! that start with 1, 9! that start with 2, etc.
+start = time.time()
+n = 10 #for digits 0-9    
+#print factorial(n)
+#3,628,800
+digs = range(0,n)
+answer = 0
+remainder = 1000000.0
+for i in range(n):
+    j = len(digs)
+    factbin = int(math.ceil(remainder/factorial(j-1)))
+    dig = digs[factbin - 1]
+    remainder -= (factbin-1) * factorial(j-1)
+    answer *= 10 #add new digit
+    answer += dig #to 10s place of answer
+    digs.remove(dig) #remove digit list to permute
+
+answer = int(answer)
+elapsed = time.time() - start
+
+print "The solution is %s, found in %s seconds." %(answer, elapsed)
+#
+
+
+
