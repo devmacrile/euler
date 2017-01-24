20ba0e311b2f50d468fa029ef8def85cff575b95
devmacrile <devin@farmlogs.com>
2017-01-23 19:24:22 -0500

    Problem 34

---
+++ solutions/python/problem34.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+"""
+
+import math
+import itertools
+from collections import Counter
+
+from eutil import clock
+
+
+digits = ''.join(map(str, range(10)))
+factorials = map(math.factorial, range(0, 10))
+
+@clock
+def faster():
+    result = 0
+    for i in range(2, 6):
+        for j in itertools.combinations_with_replacement(digits, i):
+            sum_digit_factorials = sum([factorials[int(x)] for x in j])
+            if Counter(j) == Counter([x for x in str(sum_digit_factorials)]):
+                result += sum_digit_factorials
+    return result
+
+@clock
+def main():        
+    return sum([n for n in range(3, 100000) if n == sum([factorials[int(x)] for x in str(n)])])
+
+if __name__ == '__main__':
+    faster()
+    main()
